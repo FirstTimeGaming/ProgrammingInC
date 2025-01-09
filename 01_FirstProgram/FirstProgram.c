f1dda1b384319cd1d444e7e61275442fbb1dea53
Chase Wasalaski <69854616+FirstTimeGaming@users.noreply.github.com>
2025-01-09 13:58:19 +0000

    FirstProgram

---
+++ 01_FirstProgram/FirstProgram.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// This is the first program in C
+// It prints a welcome message to the terminal
+
+// One line comment
+
+/*
+
+    This is a multiline comment
+
+*/
+
+
+// Create a main function
+
+int main(int argc, char const *argv[]) {
+
+    // Call the printf function
+
+    printf("Welcome to COP2271");
+    return 0;
+}
